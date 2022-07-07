c348dd8577b94420a24ab7ff5fe3faf82b9be1dd
william Ruiz <william.a23@gmail.com>
2022-07-07 14:37:53 -0500

    ciclo if

---
+++ If/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numeroIf = 3;
+
+        if (numeroIf > 0) {
+            System.out.println("El numero es positivo");
+        } else if (numeroIf < 0 ) {
+            System.out.println("El numero es negativo");
+        } else {
+            System.out.println("El numero es 0");   
+        }
+    }
+}
